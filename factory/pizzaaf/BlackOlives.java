371f1042a77bda538a1d97ea2907496e1efb1e0e
fwu3 <70263379+fwu3@users.noreply.github.com>
2020-08-30 11:34:30 +0800

    Update BlackOlives.java

--- factory/pizzaaf/BlackOlives.java
+++ factory/pizzaaf/BlackOlives.java
@@ -3,6 +3,7 @@ package learning.factory.pizzaaf;
 public class BlackOlives implements Veggies {
 
 	public String toString() {
+		System.out.println(" fwu3: 8/30  11:34 ");
 		return "Black Olives";
 	}
 }
