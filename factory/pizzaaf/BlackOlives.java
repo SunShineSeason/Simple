f36a9700426fbdf734e719d8bf4f699f88e8dfec
SunShineSeason <2936440956@qq.com>
2020-08-30 15:07:56 +0800

    sunshine change the file - 5

--- factory/pizzaaf/BlackOlives.java
+++ factory/pizzaaf/BlackOlives.java
@@ -2,10 +2,11 @@ package learning.factory.pizzaaf;
 
 public class BlackOlives implements Veggies {
 	
-	String str = "fwu3  username: modify to see conflics!";
+	String str = "sunshine username";
 
 	public String toString() {
 		System.out.println(" fwu3: 8/30  11:34 ");
 		return "Black Olives";
+		System.out.println(" sunshine: 8/30  11:49");
 	}
 }
