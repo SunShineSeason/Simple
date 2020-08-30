fd8ad957ff4c7dd70b901ae011f7fedc3a00d45c
fwu3 <70263379+fwu3@users.noreply.github.com>
2020-08-30 11:55:00 +0800

    fwu3 11:54 make changes

--- factory/pizzaaf/BlackOlives.java
+++ factory/pizzaaf/BlackOlives.java
@@ -1,6 +1,8 @@
 package learning.factory.pizzaaf;
 
 public class BlackOlives implements Veggies {
+	
+	String str = "fwu3 username";
 
 	public String toString() {
 		System.out.println(" fwu3: 8/30  11:34 ");
